dd27cb78890d244bb3f2301d40c4d657668a93fd
rymuelle <33radar@gmail.com>
2017-05-21 11:14:34 -0500

    added fat jets substructure

--- BSM3G_TNT_Maker/interface/JetSelector.h
+++ BSM3G_TNT_Maker/interface/JetSelector.h
@@ -100,6 +100,67 @@ public:
   vector <int> Jet_puppi_numberOfConstituents;
   vector <int> Jet_puppi_chargedMultiplicity, Jet_puppi_partonFlavour;
 
+
+  vector <double> Jet_AK8_pt, Jet_AK8_eta, Jet_AK8_phi, Jet_AK8_energy, Jet_AK8_bDiscriminator, Jet_AK8_mass;
+  vector <double> Jet_AK8_bDiscriminator_pfCISVV2, Jet_AK8_bDiscriminator_CISVV2, Jet_AK8_vtxMass, Jet_AK8_decayLength, Jet_AK8_decayLengthSignificance;
+  vector <double> Jet_AK8_pileupId, Jet_AK8_neutralHadEnergyFraction, Jet_AK8_neutralEmEmEnergyFraction; 
+  vector <double> Jet_AK8_chargedHadronEnergyFraction, Jet_AK8_chargedEmEnergyFraction, Jet_AK8_muonEnergyFraction; 
+  vector <double> Jet_AK8_electronEnergy, Jet_AK8_photonEnergy, UncorrJet_AK8_pt; 
+  vector <int> Jet_AK8_numberOfConstituents;
+  vector <int> Jet_AK8_chargedMultiplicity, Jet_AK8_partonFlavour;
+
+  
+  vector <double> Jet_AK8_tau2, Jet_AK8_tau3;
+  vector <double> Jet_AK8_puppi_pt, Jet_AK8_puppi_energy,Jet_AK8_puppi_phi, Jet_AK8_puppi_eta, Jet_AK8_puppi_mass, Jet_AK8_puppi_tau1, Jet_AK8_puppi_tau2, Jet_AK8_puppi_tau3;
+  vector <double>  Jet_AK8_subjet0_pt, Jet_AK8_subjet1_pt, Jet_AK8_subjet0_eta, Jet_AK8_subjet1_eta, Jet_AK8_subjet0_phi, Jet_AK8_subjet1_phi, Jet_AK8_subjet0_energy, Jet_AK8_subjet1_energy, Jet_AK8_subjet0_mass, Jet_AK8_subjet1_mass, Jet_AK8_subjet0_CSVv2, Jet_AK8_subjet1_CSVv2 ;
+
+  vector <double> Jet_AK8_GEN_pt, Jet_AK8_GEN_energy,Jet_AK8_GEN_phi, Jet_AK8_GEN_eta, Jet_AK8_GEN_mass, Jet_AK8_GEN_parton,Jet_AK8_GEN_mother;
+
+
+  /*
+
+
+   fChain->SetBranchStatus("jetAK8Puppi_size"             , 1 );  
+   fChain->SetBranchStatus("jetAK8Puppi_Pt"             , 1 );  
+   fChain->SetBranchStatus("jetAK8Puppi_Eta"            , 1 ); 
+   fChain->SetBranchStatus("jetAK8Puppi_Phi"            , 1 ); 
+   fChain->SetBranchStatus("jetAK8Puppi_E"            , 1 );  
+   fChain->SetBranchStatus("jetAK8Puppi_CSVv2"            , 1 );  
+   fChain->SetBranchStatus("jetAK8Puppi_GenJetE"            , 1 );  
+   fChain->SetBranchStatus("jetAK8Puppi_vSubjetIndex0"            , 1 );  
+   fChain->SetBranchStatus("jetAK8Puppi_vSubjetIndex1"            , 1 ); 
+   fChain->SetBranchStatus("jetAK8Puppi_tau2"            , 1 );  
+   fChain->SetBranchStatus("jetAK8Puppi_tau3"            , 1 ); 
+   fChain->SetBranchStatus("jetAK8Puppi_filteredMass"            , 1 ); 
+
+   fChain->SetBranchStatus("jetAK4CHS_size"             , 1 ); 
+   fChain->SetBranchStatus("jetAK4CHS_Pt"             , 1 );  
+   fChain->SetBranchStatus("jetAK4CHS_Eta"            , 1 ); 
+   fChain->SetBranchStatus("jetAK4CHS_Phi"            , 1 ); 
+   fChain->SetBranchStatus("jetAK4CHS_E"            , 1 );  
+   fChain->SetBranchStatus("jetAK4CHS_CSVv2"            , 1 );
+
+
+
+   fChain->SetBranchStatus("subjetAK8Puppi_size"            , 1 );
+   fChain->SetBranchStatus("subjetAK8Puppi_Pt"            , 1 );
+   fChain->SetBranchStatus("subjetAK8Puppi_Eta"            , 1 );
+   fChain->SetBranchStatus("subjetAK8Puppi_Phi"            , 1 );
+   fChain->SetBranchStatus("subjetAK8Puppi_CSVv2"            , 1 );
+   fChain->SetBranchStatus("subjetAK8Puppi_GenJetE"            , 1 );
+
+   fChain->SetBranchStatus("mu_size"            , 1 );
+   fChain->SetBranchStatus("mu_Pt"            , 1 );
+   fChain->SetBranchStatus("mu_Eta"            , 1 );
+   fChain->SetBranchStatus("mu_Phi"            , 1 );
+
+
+
+
+
+  */
+
+
   bool _super_TNT;
   // Jet cuts
 //  edm::InputTag jetToken_;
@@ -107,6 +168,7 @@ public:
 //  edm::InputTag _vertexInputTag;
   edm::EDGetTokenT<pat::JetCollection> jetToken_;
   edm::EDGetTokenT<pat::JetCollection> puppi_jetToken_;
+  edm::EDGetTokenT<pat::JetCollection> AK8_puppi_jetToken_;
   edm::EDGetTokenT<reco::VertexCollection> _vertexInputTag;
 
   double _Jet_pt_min;
